ac57e5a01abf772ee29fda468fbbbcc3ffdfbdd6
Sahil Sharma <sharma2003sahil8657@gmail.com>
2023-12-12 22:10:01 +0530

    ReactFlix First Commit

---
+++ flix/src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
+
+
+function Nav() {
+    const[show,handleShow]=useState(false);
+
+    useEffect(()=>{
+        window.addEventListener("scroll",()=>{
+            if(window.scrollY>100){
+                handleShow(true);
+            }else handleShow(false);
+        });
+        return ()=>{
+            window.removeEventListener("scroll",()=>{});
+        };
+    },[]);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+        <img className="nav__logo"
+        src="/netflix-logo.png"
+        alt="Netflix Logo"
+        />
+
+        <img className="nav__avatar"
+        src="/avatar.png"
+        alt="Netflix Logo"
+        />
+    </div>
+  )
+}
+
+export default Nav;
